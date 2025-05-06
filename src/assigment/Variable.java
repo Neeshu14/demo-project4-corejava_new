45444e3ea638a0f99860afdc5be20f86f0d08fb0
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-06 10:01:10 +0530

    String Integer Code

---
+++ src/assigment/Variable.java
@@ -0,0 +1,26 @@
+package assigment;
+
+public class Variable {
+    public static void main(String[] args) {
+        String name1 = "Teena";
+        String name2 = "Rahul";
+        String name3 = "Rohit";
+        String name4 = "Sumit";
+        String name5 = "Rahul";
+
+        int age1=23;
+        int age2=27;
+        int age3=28;
+        int age4=30;
+        int age5=10;
+
+        System.out.println(name1+ " "+age1);
+        System.out.println(name2+ " "+age2);
+        System.out.println(name3+ " "+age3);
+        System.out.println(name4+ " "+age4);
+        System.out.println(name5+ " "+age5);
+
+
+
+    }
+}
