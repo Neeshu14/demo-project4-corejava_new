65caabdbced8e0e88560424779177bd1c50fe464
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-06 21:21:36 +0530

    Increment

---
+++ src/variable_test/Git.java
@@ -0,0 +1,14 @@
+package variable_test;
+
+public class Git {
+    public static void main(String[] args) {
+
+        int a=8;
+
+        a++;
+        System.out.println("This is increament of 8: "+a);
+
+
+
+    }
+}
