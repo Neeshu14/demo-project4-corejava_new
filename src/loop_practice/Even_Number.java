19f255d234f8bb8a647d74f46de159aa08e244e5
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-05-28 22:10:13 +0530

    Increment

---
+++ src/loop_practice/Even_Number.java
@@ -0,0 +1,12 @@
+package loop_practice;
+
+public class Even_Number {
+    public static void main(String[] args) {
+
+        for (int i=1;i<=100;i++){
+            if (i%2 ==0){
+                System.out.println("Even Number are..."+i);
+            }
+        }
+    }
+}
