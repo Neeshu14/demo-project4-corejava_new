a78d21154636a069a1c3b7e7d30b860e073397c9
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-06-08 12:31:35 +0530

    Array Practice- day1

---
+++ src/array_/Student_Array.java
@@ -0,0 +1,15 @@
+package array_;
+
+public class Student_Array {
+
+
+    public static void main(String[] args) {
+
+        String name[]={"Neeshu","Rahul","Teena","Tanu","Abhi"};
+
+        for (int i=0;i<5;i++){
+
+            System.out.println("Printing Name :"+name[i]);
+        }
+    }
+}
