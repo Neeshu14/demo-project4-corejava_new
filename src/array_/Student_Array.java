1e5e53ccc788ceda113815986a58208a81e77756
Neeshu Bhadauriya <Nishub78@gmail.com>
2025-06-08 12:35:23 +0530

    Array Practice- day1

--- src/array_/Student_Array.java
+++ src/array_/Student_Array.java
@@ -7,9 +7,18 @@ public class Student_Array {
 
         String name[]={"Neeshu","Rahul","Teena","Tanu","Abhi"};
 
+
+        //Using For Loop
         for (int i=0;i<5;i++){
 
             System.out.println("Printing Name :"+name[i]);
+        }
+
+        //Using while Loop
+        int j=0;
+        while (j<5){
+            System.out.println("While Loop :"+name[j]);
+            j++;
         }
     }
 }
